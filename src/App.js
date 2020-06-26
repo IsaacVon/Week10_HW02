e8f14aa2db7c8d894ed93e3099a3c2c34bccaf49
Isaac Householder <isaachouseholder@gmail.com>
2020-06-26 14:47:11 -0700

    Deploy - test 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-import NavBar from './components/navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import NavBar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/pages/home";
-import About from "./components/pages/about"
-import Portfolio from "./components/pages/portfolio"
-
+import About from "./components/pages/about";
+import Portfolio from "./components/pages/portfolio";
 
 function App() {
   return (
     <Router>
       <>
         <NavBar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
       </>
     </Router>
   );
-
 }
 
 export default App;
